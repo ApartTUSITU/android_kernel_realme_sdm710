df5ae1fc528d27815a1ae90bfa8d7cbc1899e552
ApartTUSITU <aptusitu@outlook.com>
2025-10-03 22:30:27 +0800

    Revert "bpf: backport 5.4 bpf from tiffanyy's team"
    This reverts commit 7ea8187cedfc3416d91894d00d3efcf20071e3f5.

--- net/core/dst.c
+++ net/core/dst.c
@@ -386,8 +386,7 @@ static void __metadata_dst_init(struct metadata_dst *md_dst, u8 optslen)
 	memset(dst + 1, 0, sizeof(*md_dst) + optslen - sizeof(*dst));
 }
 
-struct metadata_dst *metadata_dst_alloc(u8 optslen, enum metadata_type type,
-					gfp_t flags)
+struct metadata_dst *metadata_dst_alloc(u8 optslen, gfp_t flags)
 {
 	struct metadata_dst *md_dst;
 
@@ -395,7 +394,7 @@ struct metadata_dst *metadata_dst_alloc(u8 optslen, enum metadata_type type,
 	if (!md_dst)
 		return NULL;
 
-	__metadata_dst_init(md_dst,optslen);
+	__metadata_dst_init(md_dst, optslen);
 
 	return md_dst;
 }
@@ -409,8 +408,7 @@ void metadata_dst_free(struct metadata_dst *md_dst)
 	kfree(md_dst);
 }
 
-struct metadata_dst __percpu *
-metadata_dst_alloc_percpu(u8 optslen, enum metadata_type type, gfp_t flags)
+struct metadata_dst __percpu *metadata_dst_alloc_percpu(u8 optslen, gfp_t flags)
 {
 	int cpu;
 	struct metadata_dst __percpu *md_dst;
@@ -426,22 +424,6 @@ metadata_dst_alloc_percpu(u8 optslen, enum metadata_type type, gfp_t flags)
 	return md_dst;
 }
 EXPORT_SYMBOL_GPL(metadata_dst_alloc_percpu);
-
-void metadata_dst_free_percpu(struct metadata_dst __percpu *md_dst)
-{
-#ifdef CONFIG_DST_CACHE
-	int cpu;
-
-	for_each_possible_cpu(cpu) {
-		struct metadata_dst *one_md_dst = per_cpu_ptr(md_dst, cpu);
-
-		if (one_md_dst->type == METADATA_IP_TUNNEL)
-			dst_cache_destroy(&one_md_dst->u.tun_info.dst_cache);
-	}
-#endif
-	free_percpu(md_dst);
-}
-EXPORT_SYMBOL_GPL(metadata_dst_free_percpu);
 
 /* Dirty hack. We did it in 2.2 (in __dst_free),
  * we have _very_ good reasons not to repeat
--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -2705,6 +2705,7 @@ void tcp_simple_retransmit(struct sock *sk)
 	struct tcp_sock *tp = tcp_sk(sk);
 	struct sk_buff *skb;
 	unsigned int mss = tcp_current_mss(sk);
+	u32 prior_lost = tp->lost_out;
 
 	tcp_for_write_queue(skb, sk) {
 		if (skb == tcp_send_head(sk))
@@ -2721,7 +2722,7 @@ void tcp_simple_retransmit(struct sock *sk)
 
 	tcp_clear_retrans_hints_partial(tp);
 
-	if (!tp->lost_out)
+	if (prior_lost == tp->lost_out)
 		return;
 
 	if (tcp_is_reno(tp))
@@ -5683,7 +5684,7 @@ void tcp_finish_connect(struct sock *sk, struct sk_buff *skb)
 	icsk->icsk_af_ops->rebuild_header(sk);
 
 	tcp_init_metrics(sk);
-	tcp_call_bpf(sk, BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB);
+
 	tcp_init_congestion_control(sk);
 
 	/* Prevent spurious tcp_cwnd_restart() on first data
@@ -6119,7 +6120,6 @@ int tcp_rcv_state_process(struct sock *sk, struct sk_buff *skb)
 		} else {
 			/* Make sure socket is routed, for correct metrics. */
 			icsk->icsk_af_ops->rebuild_header(sk);
-			tcp_call_bpf(sk, BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB);
 			tcp_init_congestion_control(sk);
 
 			tcp_mtup_init(sk);
@@ -6329,8 +6329,7 @@ static void tcp_ecn_create_request(struct request_sock *req,
 	ecn_ok = net->ipv4.sysctl_tcp_ecn || ecn_ok_dst;
 
 	if ((!ect && ecn_ok) || tcp_ca_needs_ecn(listen_sk) ||
-	    (ecn_ok_dst & DST_FEATURE_ECN_CA) ||
-	    tcp_bpf_ca_needs_ecn((struct sock *)req))
+	    (ecn_ok_dst & DST_FEATURE_ECN_CA))
 		inet_rsk(req)->ecn_ok = 1;
 }
 
@@ -6569,10 +6568,10 @@ int tcp_conn_request(struct request_sock_ops *rsk_ops,
 	} else {
 		tcp_rsk(req)->tfo_listener = false;
 		if (!want_cookie)
-			inet_csk_reqsk_queue_hash_add(sk, req,
-				tcp_timeout_init((struct sock *)req));
-		af_ops->send_synack(sk, dst, &fl, req,
-				    &foc, !want_cookie);
+			inet_csk_reqsk_queue_hash_add(sk, req, TCP_TIMEOUT_INIT);
+		af_ops->send_synack(sk, dst, &fl, req, &foc,
+				    !want_cookie ? TCP_SYNACK_NORMAL :
+						   TCP_SYNACK_COOKIE);
 		if (want_cookie) {
 			reqsk_free(req);
 			return 0;
